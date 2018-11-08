fa900d5631aaf60f3f9e0b37b1f7693a89a515c4
Joshua Tan <jtsarvida@gmail.com>
2018-11-08 12:36:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,5 +30,16 @@ class Student
   end
   
   def save
-    
+    sql = <<-SQL
+      INSERT INTO songs (name, grade)
+      VALUES (?, ?)
+      SQL
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 end
